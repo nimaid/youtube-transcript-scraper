f74607ad81ef9af5cb8ed0d6f1c747bb389d7d32
Ella Jameson <ellagjameson@gmail.com>
2019-12-20 19:38:28 -0700

    Update yt_scrape.py

--- yt_scrape.py
+++ yt_scrape.py
@@ -3,13 +3,13 @@ from youtube_transcript_api import YouTubeTranscriptApi
 
 API_KEY = 'YourKeyHere'
 
-def get_videos(channel_id, max_vids=50):
-    if max_vids <= 0:
-        max_vids = float('Inf')
+def get_channel_videos(channel_id, max_videos=50):
+    if max_videos <= 0:
+        max_videos = float('Inf')
     
     MAX_VIDS_PER_CALL = 50
-    if max_vids < MAX_VIDS_PER_CALL:
-        vids_per_call = max_vids
+    if max_videos < MAX_VIDS_PER_CALL:
+        vids_per_call = max_videos
     else:
         vids_per_call = MAX_VIDS_PER_CALL
     
@@ -25,8 +25,13 @@ def get_videos(channel_id, max_vids=50):
         playlist_item_res = api.get_playlist_items(playlist_id=playlist_id, count=vids_per_call, page_token=page_token)
         
         video_ids += [item.contentDetails.videoId for item in playlist_item_res.items]
-        if len(video_ids) >= max_vids:
-            video_ids = video_ids[:max_vids]
+        num_videos = len(video_ids)
+        
+        num_videos_left = max_videos - num_videos
+        if num_videos_left < MAX_VIDS_PER_CALL:
+            vids_per_call = num_videos_left
+        
+        if len(video_ids) >= max_videos:
             break
         
         page_token = playlist_item_res.nextPageToken
@@ -37,3 +42,12 @@ def get_transcript(vid_id, language='en'):
     transcript = YouTubeTranscriptApi.get_transcript(vid_id, languages=[language])
     return ' '.join([line['text'] for line in transcript]).replace('\n', '')
     
+def get_channel_transcripts(channel_id, max_videos=50, language='en'):
+    videos = get_channel_videos(channel_id=channel_id, max_videos=max_videos)
+    
+    video_transcripts = []
+    for video in videos:
+        transcript = get_transcript(video, language=language)
+        video_transcripts.append(transcript)
+    
+    return video_transcripts
