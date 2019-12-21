2a2dc63e88103f0a4aadd742f4fcb808720d6436
Ella Jameson <ellagjameson@gmail.com>
2019-12-21 10:29:21 -0700

    Basic functionality

--- yt_scrape.py
+++ yt_scrape.py
@@ -4,18 +4,26 @@ import youtube_transcript_api as _yta
 def get_channel_videos(channel_id, api_key, max_videos=50, page_token=''):
     if max_videos <= 0:
         max_videos = float('Inf')
-    
+
+    if max_videos >= 5000:
+        print('Warning: The YouTube API limits the number of requests to about 7000 videos a day.')
+        if max_videos < float('Inf'):
+            num_vids = max_videos
+        else:
+            num_vids = 'all'
+        print('You are trying to get', num_vids, 'videos, so you may hit your quota. It resets at midnight pacific time.')
+
     MAX_VIDS_PER_CALL = 50
     if max_videos < MAX_VIDS_PER_CALL:
         vids_per_call = max_videos
     else:
         vids_per_call = MAX_VIDS_PER_CALL
-    
+
     api = _pyt.Api(api_key=api_key)
     channel_res = api.get_channel_info(channel_id=channel_id)
 
     playlist_id = channel_res.items[0].contentDetails.relatedPlaylists.uploads
-    
+
     videos = {'video_ids': [], 'next_page_token': page_token}
 
     while videos['next_page_token'] is not None:
@@ -30,24 +38,24 @@ def get_channel_videos(channel_id, api_key, max_videos=50, page_token=''):
             else:
                 raise e
         videos['next_page_token'] = playlist_item_res.nextPageToken
-        
+
         videos['video_ids'] += [item.contentDetails.videoId for item in playlist_item_res.items]
         num_videos = len(videos['video_ids'])
-        
+
         num_videos_left = max_videos - num_videos
         if num_videos_left < MAX_VIDS_PER_CALL:
             vids_per_call = num_videos_left
-        
+
         if num_videos >= max_videos:
             break
 
     return videos
-    
+
 def get_transcript(video_id, language='en', type_override=None):
     transcript_object = _yta.YouTubeTranscriptApi.get_transcript(video_id, languages=(language,), type_override=type_override)
     transcript_text = ' '.join([line['text'] for line in transcript_object]).replace('\n', ' ')
     return transcript_text
-    
+
 def get_transcripts(video_ids, language='en', type_override=None):
     transcripts, temp = _yta.YouTubeTranscriptApi.get_transcripts(video_ids, languages=(language,), continue_after_error=True, type_override=type_override)
     transcript_objects = [transcripts[id] for id in transcripts.keys()]
@@ -58,3 +66,16 @@ def get_channel_transcripts(channel_id, api_key, max_videos=50, page_token='', l
     videos = get_channel_videos(channel_id=channel_id, api_key=api_key, max_videos=max_videos, page_token=page_token)
     transcripts = get_transcripts(videos['video_ids'], language=language, type_override=type_override)
     return {'video_transcripts': transcripts, 'next_page_token': videos['next_page_token']}
+
+def save_transcripts_to_file(transcripts, filename):
+    npt = transcripts['next_page_token']
+    if npt not in [None, '']:
+        filename = npt + '_' + filename
+    with open(filename, 'w') as f:
+        text = '\n'.join(transcripts['video_transcripts']).strip()
+        ascii_text = ''.join(i for i in text if ord(i)<128)
+        f.write(ascii_text)
+
+def save_channel_transcripts(channel_id, api_key, filename, max_videos=50, page_token='', language='en', type_override=None):
+    transcripts = get_channel_transcripts(channel_id, api_key, max_videos=max_videos, page_token=page_token, language=language, type_override=type_override)
+    save_transcripts_to_file(transcripts, filename)
